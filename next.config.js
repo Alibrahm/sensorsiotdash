85839da0d4e80bd09bf047fe9dda427afb430c9a
ALI IBRAHIM <alibra71@gmail.com>
2023-05-14 15:26:13 +0300

    next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     reactStrictMode: true
-// };
-// module.exports = nextConfig;
-const withTM = require("next-transpile-modules")([
-    "lodash-es",
-    "react-d3-speedometer"
-]);
+const nextConfig = {
+    reactStrictMode: true
+};
 
-module.exports = withTM();
+module.exports = nextConfig;
